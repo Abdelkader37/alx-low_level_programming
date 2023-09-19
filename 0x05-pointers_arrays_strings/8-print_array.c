c65ca42efb02c2a1a3ea50aa43eaf30286760af5
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-09-18 20:02:38 -0700

    sadija

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,47 +2,39 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
  * print_array - prints n elements of an integer array
  * @a: The array to be printed
  * @n: The number of elements to print
  */
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
-{
-if (a[i] < 0)
-{
-_putchar('-');
-a[i] = -a[i]; 
-}
-if (a[i] == 0)
-{
-_putchar('0');  
-}
-else
-{
-int num = a[i];
-int num_digits = 0;
-while (num > 0)
-{
-num /= 10;
-num_digits++;
-}
-num = a[i];
-while (num_digits > 0)
-{
-int digit = num / power(10, num_digits - 1);
-_putchar('0' + digit);
-num %= power(10, num_digits - 1);
-num_digits--;
-}
-}
-if (i < n - 1)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
- _putchar('\n');
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        /* Print the current element */
+        _putchar('0' + a[i]);
+
+        /* If it's not the last element, print a comma and a space */
+        if (i < n - 1)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+    }
+
+    /* Print a new line at the end */
+    _putchar('\n');
 }
