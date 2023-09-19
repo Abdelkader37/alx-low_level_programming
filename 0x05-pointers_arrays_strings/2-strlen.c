9e04d38132736526d2f25b7ada8fbd618eb7b85e
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-09-18 19:21:54 -0700

    lalaal

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen- output length of a string
+ *@s : pointer to string
+ *Return: length of string
+ */
+int _strlen(char *s)
+{
+int n = 0;
+while (*(s + i) != '\0')
+	i++;
+return (i);
+}
