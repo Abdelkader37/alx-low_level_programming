843eabb068314df27293412c7432db9c409dfee2
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-09-18 19:56:10 -0700

    dygf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,53 +8,43 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
-
-    for (i = 0; i < n; i++)
-    {
-        /* Handle negative numbers */
-        if (a[i] < 0)
-        {
-            _putchar('-');
-            a[i] = -a[i];  // Make the number positive for printing
-        }
-
-        /* Print the current element as digits */
-        if (a[i] == 0)
-        {
-            _putchar('0');  // Special case for 0
-        }
-        else
-        {
-            int num = a[i];
-            int num_digits = 0;
-
-            while (num > 0)
-            {
-                num /= 10;
-                num_digits++;
-            }
-
-            num = a[i];
-            while (num_digits > 0)
-            {
-                int digit = num / power(10, num_digits - 1);
-                _putchar('0' + digit);
-                num %= power(10, num_digits - 1);
-                num_digits--;
-            }
-        }
-
-        /* If it's not the last element, print a comma and a space */
-        if (i < n - 1)
-        {
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-
-    /* Print a new line at the end */
-    _putchar('\n');
+int i;
+for (i = 0; i < n; i++)
+{
+if (a[i] < 0)
+{
+_putchar('-');
+a[i] = -a[i]; 
+}
+if (a[i] == 0)
+{
+_putchar('0');  
+}
+else
+{
+int num = a[i];
+int num_digits = 0;
+while (num > 0)
+{
+num /= 10;
+num_digits++;
+}
+num = a[i];
+while (num_digits > 0)
+{
+int digit = num / power(10, num_digits - 1);
+_putchar('0' + digit);
+num %= power(10, num_digits - 1);
+num_digits--;
+}
+}
+if (i < n - 1)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+ _putchar('\n');
 }
 
 /**
@@ -66,11 +56,11 @@ void print_array(int *a, int n)
  */
 int power(int base, int exp)
 {
-    int result = 1;
-    while (exp > 0)
-    {
-        result *= base;
-        exp--;
-    }
-    return result;
+int result = 1;
+while (exp > 0)
+{
+result *= base;
+exp--;
+}
+return result;
 }
