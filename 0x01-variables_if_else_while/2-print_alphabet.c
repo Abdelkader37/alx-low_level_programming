de2e69a3cad5f816aa51ac14c767a9082bfc401d
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-10-01 09:23:40 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+  /**
+   * Description: print alphabet
+   *
+   * Return: Returns 0 always.
+   */
+
+int main ()
+{
+    char n = 'a' ;
+
+        while (n >= 'a' && n <= 'z')
+           {
+           putchar (n);
+            n++ ;
+           }
+           putchar ('\n');
+            return (0) ;
+}
