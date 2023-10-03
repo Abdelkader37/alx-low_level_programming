90e9fbaac3009fa3c0a1d1c5f1f95f08351cc49e
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-10-03 11:37:39 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - start point
+*Discription: prent alph expect e and q
+*return :always returne to 0
+**/
+
+int main (void)
+{
+	char n = 'a';
+	while (n <= 'z' && n != 'q' && n != 'e')
+	{
+	putchar (n);
+	n++;
+	}
+	putchar (\n);
+
+	return (0);
+}
