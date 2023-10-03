c575e0b22354bd23ca1c3583b4ac24caad055f6e
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2023-10-03 15:09:18 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main -start point
+ *
+ * Description: single-digit numbers in base 10 start from 0 and go up to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	while (n < 10)
+	{
+	printf("%d",n);
+	n++;
+	}
+	return (0);
+}
