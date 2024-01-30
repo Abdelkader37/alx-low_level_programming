ff4ba3017c66179142a08b8655bfcac2deeadc70
Qrafi Abdelkader <qrafiabdelkader@gmail.com>
2024-01-30 01:53:03 +0100

    done all tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https:/Abdelkader37/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -p /tmp https:/Abdelkader37/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/lilgiga.so
